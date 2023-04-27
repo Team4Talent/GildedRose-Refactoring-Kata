c80d0489bf9b45a5db19977d2b80c2b1f599531e
Kim Van Renterghem <kim.vanrenterghem@t4t.rocks>
2023-04-27 16:38:00 +0200

    fix build and move to dotnet core

--- csharp/GildedRoseCalculator.Test/ApprovalTest.cs
+++ csharp/GildedRoseCalculator.Test/ApprovalTest.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using NUnit.Framework;
 
-namespace csharp
+namespace ConsoleApp1
 {
     [UseReporter(typeof(DiffReporter))]
     [TestFixture]
---
+++ csharp/GildedRoseCalculator.Test/GildedRoseTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace GildedRoseCalculator.Test;
+
+[TestFixture]
+public class GildedRoseTest
+{
+    [Test]
+    public void foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.AreEqual("fixme", Items[0].Name);
+    }
+}
